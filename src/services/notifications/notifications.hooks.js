c4d7aca0820cc39a611df4b39fafcfef404b9d87
Vitor George <vitor.george@aparabolica.com.br>
2018-04-03 15:02:04 +0100

    Add push notifications

--- src/services/notifications/notifications.hooks.js
+++ src/services/notifications/notifications.hooks.js
@@ -1,6 +1,7 @@
 const { authenticate } = require("@feathersjs/authentication").hooks;
 const { restrictToRoles, associateCurrentUser } = require('feathers-authentication-hooks');
-const { iff, isProvider } = require("feathers-hooks-common");
+const { iff, isProvider, getItems } = require("feathers-hooks-common");
+const { scheduleNotification } = require('../../lib/push-notifications');
 
 const restrict = [
   iff(isProvider('external'), [
@@ -19,8 +20,32 @@ module.exports = {
     all: [],
     find: [authenticate("jwt")],
     get: [],
-    create: [...restrict, associateCurrentUser({ idField: "id", as: "senderId" })],
+    create: [...restrict, associateCurrentUser({ idField: "id", as: "senderId" }), registerPushNotification()],
     remove: [...restrict]
   },
 
 };
+
+function registerPushNotification() {
+  return function (hook) {
+    const { recipientId, title, data, deliveryTime, type } = hook.data;
+
+    switch (type) {
+      case "sample-analysis-finished":
+        scheduleNotification(
+          recipientId,
+          {
+            deeplink: 'sample/' + data.sampleId,
+            message: title
+          },
+          deliveryTime,
+          err => {
+            if (err) console.log(err);
+          });
+        break;
+
+      default:
+        break;
+    }
+  }
+}
