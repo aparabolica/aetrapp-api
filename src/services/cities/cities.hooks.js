e0430b44efc6506e124a3f8b9fb79329db926841
Vitor George <vitor.george@aparabolica.com.br>
2018-04-11 17:25:19 -0300

    Remove eggMeanSeries resolver from cities service

--- src/services/cities/cities.hooks.js
+++ src/services/cities/cities.hooks.js
@@ -2,54 +2,6 @@ const _ = require('lodash');
 const moment = require('moment');
 const { disallow, fastJoin } = require('feathers-hooks-common');
 
-const cityResolver = {
-  joins: {
-    eggMeanSeries: (...args) => async (city, context) => {
-      const traps = context.app.services['traps'];
-      const samples = context.app.services['samples'];
-
-      const trapIds = (await traps.find({
-        query: {
-          $select: ['id'],
-          cityId: city.id
-        },
-        paginate: false
-      }));
-
-      let validSamples = (await samples.find({
-        query: {
-          trapId: trapIds.map(trap => trap.id),
-          status: 'valid',
-          $select: ['id', 'eggCount', 'collectedAt']
-        },
-        paginate: false
-      }));
-
-      validSamples = validSamples.map(sample => {
-        const collectedAt = moment(sample.collectedAt);
-        sample.week = collectedAt.format('YYYY-w');
-        return sample;
-      });
-
-      let weeks = _.groupBy(validSamples, 'week');
-
-      for (const weekId in weeks) {
-        if (weeks.hasOwnProperty(weekId)) {
-          weeks[weekId] = _.meanBy(weeks[weekId], 'eggCount');
-        }
-      }
-
-      city.eggMeanSeries = weeks;
-
-      // set current week mean count
-      const currentWeek = moment().format('YYYY-w');
-      if (weeks.hasOwnProperty(currentWeek)) {
-        city.currentMeanCount = weeks[currentWeek];
-      }
-    }
-  }
-}
-
 module.exports = {
   before: {
     find: [],
@@ -60,7 +12,5 @@ module.exports = {
     remove: disallow()
   },
   after: {
-    find: [fastJoin(cityResolver)],
-    get: [fastJoin(cityResolver)],
   }
 };
