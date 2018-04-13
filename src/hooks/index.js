67a6f96371ddb8150e4931a1b68f10f8cbaeb673
Vitor George <vitor.george@aparabolica.com.br>
2018-04-13 13:29:07 -0300

    add updateCityStatistics to hook index

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,6 +3,8 @@ module.exports = {
   sendVerificationEmail: require('./send-verification-email.js'),
   setFirstUserToRole: require('./set-first-user-to-role'),
   updateUserTrapCount: require('./update-user-trap-count'),
+  // cities
+  updateCityStatistics: require('./cities/update-city-statistics'),
 }
 
 
