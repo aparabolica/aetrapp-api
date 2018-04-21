3a166e5d5dcd983498dd8866997b50944249dbdb
Vitor George <vitor.george@aparabolica.com.br>
2018-04-21 16:39:25 -0300

    Remove deprecated traps csv hook

--- src/hooks/traps/index.js
+++ src/hooks/traps/index.js
@@ -2,6 +2,5 @@ module.exports = {
   updateUserTrapCount: require('./update-user-trap-count'),
   setActiveTrapStatus: require('./set-active-trap-status'),
   setCycleDates: require('./set-cycle-dates'),
-  generateTrapsCSV: require('./generate-traps-csv'),
   parseSortByCity: require('./parse-sort-by-city')
 }
